43df2f6107e6f2d1117f28751e9db2dec0619489
Pablo Castro <pablocastrodb@gmail.com>
2024-10-24 23:04:20 +0200

    Initial Commit

---
+++ Tempora/TemporaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemporaApp.swift
+//  Tempora
+//
+//  Created by Pablo Castro on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TemporaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
